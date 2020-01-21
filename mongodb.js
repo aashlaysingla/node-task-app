6272467bc80a32c8a9db9e47d50607f17b7dad96
Aashlay <aashlaysingla@gmail.com>
2020-01-22 00:44:30 +0530

    third commit

--- mongodb.js
+++ mongodb.js
@@ -3,16 +3,11 @@ const {MongoClient, ObjectID} = require('mongodb')
 const connectionUrl = "mongodb://127.0.0.1:27017"
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp())
-
 MongoClient.connect(connectionUrl, { useNewUrlParser: true}, (error, client) => {
         if (error) {
             return console.log('Unable to connect to database')
         }
 
-        // console.log('Connected correctly')
         const db = client.db(databaseName)
 
         // db.collection('users').insertOne({
@@ -60,6 +55,8 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true}, (error, client) => 
 
         //     console.log(result.ops)
         // })
+
+        
 
 
 })
